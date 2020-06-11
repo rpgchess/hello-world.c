43bf94d6882b30625ea1bbb1a9deedd45474126a
Leticia Caroline Leonardo <leticiacaroline-123@hotmail.com>
2020-06-11 18:18:53 -0300

    adicionando novos exercicios

---
+++ Iniciante/area-do-circulo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    double area, raio;
+    double n = 3.14159;
+
+    scanf("%lf", &raio);
+
+    area = (n * (raio*raio));
+
+    printf("A=%.4lf\n", area);
+
+    return 0;
+}
---
+++ Iniciante/media-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  double A, B, C, MEDIA;
+
+  scanf("%lf %lf %lf", &A, &B, &C);
+
+  MEDIA = (((A*2)+(B*3)+(C*5))/10);
+
+  printf("MEDIA = %.1f\n", MEDIA);
+
+  return 0;
+}
